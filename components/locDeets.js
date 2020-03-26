d0b9313a545773d8a9925ce70fb96aaafba4920f
Eric Lowry <ericmichaellowry@gmail.com>
2020-03-26 12:35:22 -0700

    Converted data table to single rows, imported bold font version for th elements

--- components/locDeets.js
+++ components/locDeets.js
@@ -15,22 +15,29 @@ class LocDeets{
     this.deetsElement.innerHTML = "";
     console.log(results.status);
 
+    var latLabelRow = document.createElement("tr");
     var latRow = document.createElement("tr");
     var latHead = document.createElement("th");
     var latData = document.createElement("td");
     latHead.textContent = "Latitude";
     latData.textContent = latitude;
-    latRow.appendChild(latHead);
+    latHead.classList.add("text-center");
+    latData.classList.add("text-center");
+    latLabelRow.appendChild(latHead);
     latRow.appendChild(latData);
 
+    var longLabelRow = document.createElement("tr");
     var longRow = document.createElement("tr");
     var longHead = document.createElement("th");
     var longData = document.createElement("td");
     longHead.textContent = "Longitude";
     longData.textContent = longitude;
-    longRow.appendChild(longHead);
+    longHead.classList.add("text-center");
+    longData.classList.add("text-center");
+    longLabelRow.appendChild(longHead);
     longRow.appendChild(longData);
 
+    var locLabelRow = document.createElement("tr");
     var locRow = document.createElement("tr");
     var locHead = document.createElement("th");
     var locData = document.createElement("td");
@@ -43,11 +50,16 @@ class LocDeets{
       locData.textContent = "Remote Location or Ocean, No Google Data Available";
     }
 
-    locRow.appendChild(locHead);
+    locHead.classList.add("text-center");
+    locData.classList.add("text-center");
+    locLabelRow.appendChild(locHead);
     locRow.appendChild(locData);
 
+    this.deetsElement.appendChild(latLabelRow);
     this.deetsElement.appendChild(latRow);
+    this.deetsElement.appendChild(longLabelRow);
     this.deetsElement.appendChild(longRow);
+    this.deetsElement.appendChild(locLabelRow);
     this.deetsElement.appendChild(locRow);
   }
 
@@ -58,9 +70,8 @@ class LocDeets{
     var photoCell = document.createElement("td");
     var photo = document.createElement("img");
 
-    photoLabelRow.colSpan = "2";
     photoLabelRow.classList.add("text-center");
-    photoRow.colSpan = "2";
+    photoRow.classList.add("text-center");
     photoHead.textContent = "Location Photo";
     photo.src = photoUrl;
 
